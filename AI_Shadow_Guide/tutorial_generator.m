0b17c2d955daa1f0cc6645b18045113847746b54
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-05-22 08:46:34 +0300

    Add tutorial generator function in tutorial_generator.m

---
+++ AI_Shadow_Guide/tutorial_generator.m
@@ -0,0 +1,5 @@
+// tutorial_generator.m - مولد الشروحات المصورة (إطار العمل)
+#import <Foundation/Foundation.h>
+void generate_tutorial(NSString *cve) {
+    NSLog(@"Generating interactive tutorial for %@", cve);
+}
